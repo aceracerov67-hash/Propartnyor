0ad46e954d7956f56e184f00e3555bd2d9d7ee18
farzona <aceracerov67@gmail.com>
2026-01-30 03:25:37 +0500

    change one

--- src/Components/Hero/Destinations/Destinations.jsx
+++ src/Components/Hero/Destinations/Destinations.jsx
@@ -1,11 +1,13 @@
-
 import React from "react";
 import { useTranslation } from "react-i18next";
+import dubai from './images/dubai2.jpg'
+import istanbul from './images/istanbul2.jpg'
+import sharm from './images/sharm2.jpg'
 
 const images = [
-  "https://images.unsplash.com/photo-1504274066651-8d31a536b11a",
-  "https://images.unsplash.com/photo-1527838832700-5059252407fa",
-  "https://images.unsplash.com/photo-1507525428034-b723cf961d3e",
+  dubai,
+  istanbul,
+  sharm
 ];
 
 const stars = [5, 5, 4];
@@ -16,7 +18,7 @@ const Destinations = () => {
 
   return (
     <section className="max-w-7xl mx-auto px-4 py-16">
-      {/* Header */}
+     
       <div className="text-center mb-12">
         <p className="text-blue-500 text-sm mb-2">
           {t("destinations.sectionTitle")}
@@ -26,7 +28,7 @@ const Destinations = () => {
         </h2>
       </div>
 
-      {/* Cards */}
+     
       <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
         {cards.map((item, index) => (
           <div
@@ -43,7 +45,6 @@ const Destinations = () => {
               hover:shadow-2xl
             "
           >
-            {/* Image */}
             <div className="relative overflow-hidden">
               <img
                 src={images[index]}
@@ -58,7 +59,7 @@ const Destinations = () => {
                 "
               />
 
-              {/* Overlay */}
+             
               <div className="
                 absolute inset-0
                 bg-black/20
@@ -68,7 +69,7 @@ const Destinations = () => {
                 group-hover:opacity-100
               "></div>
 
-              {/* Stars */}
+            
               <div className="absolute top-4 left-4 flex gap-1 text-white z-10">
                 {Array.from({ length: stars[index] }).map((_, i) => (
                   <span key={i}>★</span>
@@ -76,7 +77,6 @@ const Destinations = () => {
               </div>
             </div>
 
-            {/* Content */}
             <div className="p-6 transition-all duration-500 group-hover:bg-gray-50">
               <h3 className="text-xl font-semibold mb-2 transition-colors duration-300 group-hover:text-blue-600">
                 {item.title}
@@ -89,7 +89,6 @@ const Destinations = () => {
               </p>
             </div>
 
-            {/* Buttons */}
             <div className="border-t grid grid-cols-2 text-sm">
               <button className="
                 py-3
